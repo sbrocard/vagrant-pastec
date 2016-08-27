4f36f4c21611c3c66d23a8e48ecf48369ac3c5ba
sbrocard <sbrocard@users.noreply.github.com>
2016-08-27 18:31:11 +0100

    vagrant pastec

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VISUAL_WORD_LIST_PATH=/pastec/visualWordsORB.dat
+
+set -e
+
+#ln /dev/null /dev/raw1394
+
+if [ "$1" = 'pastec' ]; then
+    if [ -n "${!2}" ]; then
+        exec pastec ${!2}
+    elif [ -n "$2" ]; then
+        exec pastec $2
+    else
+        exec pastec $VISUAL_WORD_LIST_PATH
+    fi
+fi
+
+exec "$@"
+
---
+++ pastec.sh
@@ -0,0 +1,1 @@
+vagrant ssh -c "source /entrypoint.sh pastec"
